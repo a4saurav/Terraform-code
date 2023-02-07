c0818ef7d4286b978f7ab31109aca2719b9d1874
Avinash Saurav <68444685+a4saurav@users.noreply.github.com>
2023-02-07 19:20:14 +0530

    Code Corrected

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "github" {
   token = "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"
 }
 
-resource "github_repository" "terraform repo" {
+resource "github_repository" "terraform-repo" {
   name        = "terraform first repo"
   description = "My first repo from terraform"
 
